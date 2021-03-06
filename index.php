aafe8e8156ef9ff56343cf802a894eb2712ed794
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2021-03-06 22:58:39 +0100

    Index file Added

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hello! Welcome to our Authentication web app.</h1>
+    </div>
+    <h2>
+    <a href="register.php" class="btn btn-primary">Register</a>
+        <a href="login.php" class="btn btn-success">Sign in</a>
+    </h2>
+</body>
+</html>
